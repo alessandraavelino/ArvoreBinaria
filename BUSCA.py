b8a11a5516d9f18c5a46ee0be74db50481ed82a1
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2021-10-25 19:59:37 -0300

    Add files via upload

---
+++ BUSCA.py
@@ -0,0 +1,30 @@
+def buscaBinaria(vetor, key, i = 0, F = None):
+    if (F == None):
+        F = len(vetor) -1
+
+    if (F < i):
+        return -1
+    
+    meio = (i + F) // 2
+    if (vetor[meio] == key):
+        return meio
+    
+    if (vetor[meio] > key):
+        F = meio -1
+    else:
+        i = meio +1
+    return buscaBinaria(vetor, key, i, F)
+
+# Test array
+
+vetor = [ 2, 3, 4, 10, 40 ]
+print('Digite a chave de busca: ')
+key = int(input('>> '))
+
+resultado = buscaBinaria(vetor, key, 0, len(vetor)-1)
+
+if resultado != -1:
+	print("Elemento presente na posição", str(resultado))
+else:
+	print("Elemento não está presente no vetor")
+
